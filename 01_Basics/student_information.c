facba4bf6deb68636afa951c058731f9b0b0d337
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-17 20:27:40 +0530

    Second print problem.

---
+++ 01_Basics/student_information.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("----Student Profile----\n Name: \t Sujatha Selvaraj \n Age: \t 17 \n Course:\t M.s.c DataScience \n Year: \t 2025 \n \"Keep Learning!\"");
+    return 0;
+}
