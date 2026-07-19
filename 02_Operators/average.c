e166120e2851d5613d9902e852d0acd7d144551a
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-19 17:06:09 +0530

    Cube

--- 02_Operators/average.c
+++ 02_Operators/average.c
@@ -1,5 +1,4 @@
 //Calculate the average of three numbers.
-
 #include <stdio.h>
 int main () {
     int num1, num2, num3;
---
+++ 02_Operators/cube.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1;
+    printf("Enter a number:");
+    scanf("%d", &num1);
+    printf("The cube of %d is %d", num1, (num1*num1*num1));
+
+    return 0;
+}
