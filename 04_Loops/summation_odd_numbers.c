14b9790e6f5a22c27b361666d2692ca2df25b980
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-22 06:52:23 +0530

    Odd summation numbers.

---
+++ 04_Loops/summation_odd_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++)
+    if (i%2!=0){
+        printf("%d+%d=%d\n", n, i, n+i);
+    }
+    return 0;
+}
