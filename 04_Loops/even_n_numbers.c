631d66718955d6d91eaf29df9eb6efdeeabc946f
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-22 06:27:30 +0530

    Even number.

---
+++ 04_Loops/even_n_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++)
+    if (i%2==0){
+        printf("%d\n", i);
+    }
+    return 0;
+}
