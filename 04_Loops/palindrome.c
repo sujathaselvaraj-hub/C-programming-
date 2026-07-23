964aa752dfbf81a2f0fd8c12fc5f344efd8e467d
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-23 07:11:44 +0530

    Palindrome

---
+++ 04_Loops/palindrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int n, reversed=0,remainder,original;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    original=n;
+    while (n!=0){
+        remainder=n%10;
+        reversed=reversed*10 +remainder;
+        n=n/10;
+    }
+    if (original==reversed)
+    {printf("%d is a palindrome.\n", original);}
+    else{
+        ("%d is not a palindrome.\n", original);
+    }
+    return 0;
+}
