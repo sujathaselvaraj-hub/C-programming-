25b3e9c5a0cc7b16e071f6e10a85491bb3d1affb
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-08-20 19:25:51 +0530

    new reverse commit.

--- 05_functions/reverse_number.c
+++ 05_functions/reverse_number.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 int reverseNum(int n){
     int digit, reverse=0;
     while(n!=0){
@@ -18,25 +18,3 @@ int main(){
     return 0;
 }
 
-/*#include <stdio.h>
-int reverseNumber(int n)
-{
-int reverse = 0;
-int digit;
-while (n != 0)
-{
-digit = n % 10;
-reverse = reverse * 10 + digit;
-n = n / 10;
-}
-return reverse;
-}
-int main()
-{
-int number, result;
-printf("Enter a number:");
-scanf("%d", &number);
-result = reverseNumber(number);
-printf("Reversed Number = %d", result);
-return 0;
-}*/
